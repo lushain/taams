1a90ac2bfb21eda02e1a8d6e0be9525a9ef44bd8
Vyom <vyom262327@gmail.com>
2025-11-19 23:14:12 +0530

    second

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="py-4 text-center text-gray-400 text-sm mt-auto">
-      Made by Ryan & Abhijit
+      Made by Harehar & Anushka
     </footer>
   );
 }
